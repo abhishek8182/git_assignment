df6019d71bf1b7678d7f02bac31da86cf2c3dce8
abhishek8182 <abhishektripathi3196@gmail.com>
2022-12-09 20:53:21 +0530

    Prime and pallindrome

---
+++ pallidrome.js
@@ -0,0 +1,14 @@
+function pallindrome(str){
+	let rev=""
+
+	for(let i=str.length-1; i>=0; i--){
+		rev+=str[i]
+	}
+
+	if(){						//str==rev
+		console.log("Number is Pallindrome ")
+	}else{
+		console.log("Not Pallindrome")
+	}
+
+}
